3827151474567bc4106b860519bcc19c2e76c9f0
Andrew Nater <a.a.nater@gmail.com>
2015-11-11 13:22:08 -0500

    DRY up codebase

---
+++ checkout.php
@@ -0,0 +1,20 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<?php include '_components/head.php'; ?>
+<body>
+	<?php include '_components/header.php'; ?>
+	<div class="login">
+		<div class="wrap">
+			<h4 class="title">Shopping cart is empty</h4>
+			<p class="cart">You have no items in your shopping cart.<br>Click<a href="index.html"> here</a> to continue shopping</p>
+		</div>
+	</div>
+	<?php include '_components/footer.php'; ?>     
+</body>
+</html>
